514250cb7f9dfc12829d1c6103c1b7babb1835ee
Andrew Budziszek <abudziszek@gmail.com>
2020-01-15 14:22:23 -0600

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,38 @@
 <script>
   import { onMount } from "svelte";
-  export let date;
 
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
-  });
+  function goToLinkedIn() {
+    window.location.href =
+      "https://www.linkedin.com/in/andrew-budziszek-ssm-929a1663";
+  }
+
+  function goToGitHub() {
+    window.location.href = "https://github.com/SonnyBrooks";
+  }
+
+  function goToTwitter() {
+    window.location.href = "https://twitter.com/AndrewBudziszek";
+  }
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://zeit.co/docs" target="_blank" rel="noreferrer noopener">
-      ZEIT Now
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/zeit/now-examples/tree/master/svelte-functions"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <section class="wrapper">
+    <div id="stars" />
+    <div id="stars2" />
+    <div id="stars3" />
+    <div id="title">
+      <span>BUDZISZEK</span>
+      <br />
+      <span>(BUD-ziz-ek)</span>
+      <br />
+      <div class="nameplate">
+        <span class="small link" on:click={goToLinkedIn}>LinkedIn</span>
+        <span class="small">|</span>
+        <span class="small link">GitHub</span>
+        <span class="small">|</span>
+        <span class="small link">Twitter</span>
+      </div>
+      <span class="small link">andrew@budziszek.com</span>
+    </div>
+  </section>
 </main>
